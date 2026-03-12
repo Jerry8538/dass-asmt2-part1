283a76645188d895ab283bbb7f61aeba810bedeb
yk <jerr8538@gmail.com>
2026-03-12 22:57:30 +0530

    Iteration 6: dice.py and cards.py: long lines and other bs

--- moneypoly/dice.py
+++ moneypoly/dice.py
@@ -1,5 +1,5 @@
+"""deals with dice rolls"""
 import random
-from moneypoly.config import BOARD_SIZE
 
 class Dice:
     """Simulates a pair of six-sided dice with consecutive-doubles tracking."""
@@ -7,7 +7,8 @@ class Dice:
     def __init__(self):
         self.die1 = 0
         self.die2 = 0
-        self.reset()
+        self.doubles_streak = 0
+        self.reset() # TODO: useless?
 
     def reset(self):
         """Reset dice face values and the doubles streak counter."""
